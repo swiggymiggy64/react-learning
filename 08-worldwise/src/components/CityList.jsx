2a2f4a5ae3ce9dbc3dd2884ee59922ca3243798a
swiggymiggy64 <michaelwilliams6@hotmail.com>
2023-07-23 09:00:26 +0100

    Update CityList.jsx
    Included information to wait for API to reactivate.

--- 08-worldwise/src/components/CityList.jsx
+++ 08-worldwise/src/components/CityList.jsx
@@ -3,11 +3,22 @@ import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
 import { useCities } from "../context/CitiesContext";
+import { Link } from "react-router-dom";
 
 function CityList() {
   const { cities, isLoading } = useCities();
 
-  if (isLoading) return <Spinner />;
+  if (isLoading)
+    return (
+      <>
+        <h1>
+          The API used in this demo is deactivated if unused after 15 minutes &
+          will need a little time to reactivate. Please wait while the API is
+          reactivated or click <Link to="/">here</Link> to refresh the page.
+        </h1>
+        <Spinner />
+      </>
+    );
 
   if (!cities.length)
     return (
