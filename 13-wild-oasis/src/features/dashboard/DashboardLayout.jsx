6cac9c05873e6a2af4400bdfae998b0b4a4c765a
swiggymiggy64 <michaelwilliams6@hotmail.com>
2023-07-27 16:11:02 +0100

    Dashboard & Error Boundary complete
    Dashboard modules & Error Boundaries completed

--- 13-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 13-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,12 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import { useCabins } from "./../cabins/useCabins";
+import Stats from "./Stats";
+import Spinner from "./../../ui/Spinner";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,12 +16,23 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { confirmedStays, isLoading: isLoading2, numDays } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
+
+  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
-      <div>Stats</div>
-      <div>Today's activity</div>
-      <div>Chart stay duration</div>
-      <div>Chart sales</div>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <TodayActivity />
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
