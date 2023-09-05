e6ed0ee9476c3c28592dd03a58c10107a5beb792
swiggymiggy64 <michaelwilliams6@hotmail.com>
2023-09-05 12:37:52 +0100

    Adding redirects

--- 13-wild-oasis/src/App.jsx
+++ 13-wild-oasis/src/App.jsx
@@ -60,6 +60,7 @@ function App() {
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </BrowserRouter>
+
         <Toaster
           position="top-center"
           gutter={12}
